be6992b5b679fc6671f5120bbbaa2af6dd0963d7
LokeshKvms <loki.kvms@gmail.com>
2025-05-15 11:59:44 +0530

    ...

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+define('BASE_PATH', "C:/xampp/htdocs/elms");
+// echo BASE_PATH;
+
+define('EMPL_PATH', BASE_PATH . '/employee');
+define('ADMIN_PATH', BASE_PATH . '/admin');
+define('INCLUDES_PATH', BASE_PATH . '/includes'); 
+define('AUTH_PATH', BASE_PATH.'/auth');
+define('COMMON_PATH', BASE_PATH . '/common');
+
+$baseUrl = "http://localhost/elms";
+define('BASE_URL', $baseUrl);
+// echo BASE_URL;
