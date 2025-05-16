d083f39e789ce516b00d6900e4c6561c88064a14
LokeshKvms <loki.kvms@gmail.com>
2025-05-16 12:40:46 +0530

    ...

--- config.php
+++ config.php
@@ -1,13 +1,10 @@
 <?php
-define('BASE_PATH', "C:/xampp/htdocs/elms");
-// echo BASE_PATH;
-
+define('BASE_PATH', dirname(__DIR__));
 define('EMPL_PATH', BASE_PATH . '/employee');
 define('ADMIN_PATH', BASE_PATH . '/admin');
 define('INCLUDES_PATH', BASE_PATH . '/includes'); 
 define('AUTH_PATH', BASE_PATH.'/auth');
 define('COMMON_PATH', BASE_PATH . '/common');
 
-$baseUrl = "https://elms-production.up.railway.app";
+$baseUrl = "http://localhost/elms";
 define('BASE_URL', $baseUrl);
-// echo BASE_URL;
